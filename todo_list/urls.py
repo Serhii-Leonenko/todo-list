eb3733e467e62635991baa697400a0a4314c13fb
Serhii Leonenko <leonenko3149@gmail.com>
2022-07-11 14:47:52 +0300

    static added

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,10 +1,12 @@
 """
 todo_list URL Configuration
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
